b16547444d652493192aa5e06f9e4157308dce71
wulaxixi <752713947@qq.com>
2018-03-26 07:23:10 +0800

    提交

--- TestProject2016051/src/mycountprogram/EggsCount.java
+++ TestProject2016051/src/mycountprogram/EggsCount.java
@@ -2,19 +2,11 @@ package mycountprogram;
 
 public class EggsCount {
 	
+	public EggsCount() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	/**
-<<<<<<< HEAD
-	 * è®¡ç®—å­˜æ”¾å°äºŽåä¸‡ä¸ªé¸¡è›‹çš„ç¯®å­ï¼Œå¯ä»¥æ»¡è¶³ä¸€ä¸‹è§„åˆ™ï¼š<br>
-	 * 1ä¸ª1ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œæ•´å¥½æ‹¿å®Œï¼›<br>
-	 * 2ä¸ª2ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œè¿˜å‰©1ä¸ªï¼›<br>
-	 * 3ä¸ª3ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œæ•´å¥½æ‹¿å®Œï¼›<br>
-	 * 4ä¸ª4ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œè¿˜å‰©1ä¸ªï¼›<br>
-	 * 5ä¸ª5ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œè¿˜å‰©1ä¸ªï¼›<br>
-	 * 6ä¸ª6ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œè¿˜å‰©3ä¸ªï¼›<br>
-	 * 7ä¸ª7ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œæ•´å¥½æ‹¿å®Œï¼›<br>
-	 * 8ä¸ª8ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œè¿˜å‰©1ä¸ªï¼›<br>
-	 * 8ä¸ª8ä¸ªçš„æ‹¿å‡ºé¸¡è›‹ï¼Œæ•´å¥½æ‹¿å®Œã€‚
-=======
 	 * ¼ÆËã´æ·ÅÐ¡ÓÚÊ®Íò¸ö¼¦µ°µÄÀº×Ó£¬¿ÉÒÔÂú×ãÒ»ÏÂ¹æÔò£º<br>
 	 * 1¸ö1¸öµÄÄÃ³ö¼¦µ°£¬ÕûºÃÄÃÍê£»<br>
 	 * 2¸ö2¸öµÄÄÃ³ö¼¦µ°£¬»¹Ê£1¸ö£»<br>
@@ -25,7 +17,6 @@ public class EggsCount {
 	 * 7¸ö7¸öµÄÄÃ³ö¼¦µ°£¬ÕûºÃÄÃÍê£»<br>
 	 * 8¸ö8¸öµÄÄÃ³ö¼¦µ°£¬»¹Ê£1¸ö£»<br>
 	 * 8¸ö8¸öµÄÄÃ³ö¼¦µ°£¬ÕûºÃÄÃÍê¡£
->>>>>>> d4b1f762158692e3a8fec5e5fa07c4081ae5bd4a
 	 * @param args
 	 */
 	public static void main(String[] args){
@@ -38,35 +29,17 @@ public class EggsCount {
 			if(i%7!=0)continue;
 			if((i-1)%8!=0)continue;
 			if(i%9!=0)continue;
-<<<<<<< HEAD
-			System.err.println("é¸¡è›‹ä¸ªæ•°ï¼š"+i+";\n"+appendMessage(i));
-=======
 			System.err.println("¼¦µ°¸öÊý£º"+i+";\n"+appendMessage(i));
->>>>>>> d4b1f762158692e3a8fec5e5fa07c4081ae5bd4a
 		}
 	}
 	
 	/**
-<<<<<<< HEAD
-	 * æ‹¼æŽ¥æ‹¿å–æ¬¡æ•°ä¿¡æ¯
-=======
 	 * Æ´½ÓÄÃÈ¡´ÎÊýÐÅÏ¢
->>>>>>> d4b1f762158692e3a8fec5e5fa07c4081ae5bd4a
 	 * @param i
 	 * @return
 	 */
 	private static String appendMessage(int i){
 		StringBuilder message = new StringBuilder();
-<<<<<<< HEAD
-		message.append("2ä¸ªçš„æ‹¿è¦"+(i-1)/2+"æ¬¡ï¼›");
-		message.append("3ä¸ªçš„æ‹¿è¦"+(i)/3+"æ¬¡ï¼›");
-		message.append("4ä¸ªçš„æ‹¿è¦"+(i-1)/4+"æ¬¡ï¼›");
-		message.append("5ä¸ªçš„æ‹¿è¦"+(i-1)/5+"æ¬¡ï¼›");
-		message.append("6ä¸ªçš„æ‹¿è¦"+(i-3)/6+"æ¬¡ï¼›");
-		message.append("7ä¸ªçš„æ‹¿è¦"+(i)/7+"æ¬¡ï¼›");
-		message.append("8ä¸ªçš„æ‹¿è¦"+(i-1)/8+"æ¬¡ï¼›");
-		message.append("9ä¸ªçš„æ‹¿è¦"+(i)/9+"æ¬¡ã€‚");
-=======
 		message.append("2¸öµÄÄÃÒª"+(i-1)/2+"´Î£»");
 		message.append("3¸öµÄÄÃÒª"+(i)/3+"´Î£»");
 		message.append("4¸öµÄÄÃÒª"+(i-1)/4+"´Î£»");
@@ -75,7 +48,6 @@ public class EggsCount {
 		message.append("7¸öµÄÄÃÒª"+(i)/7+"´Î£»");
 		message.append("8¸öµÄÄÃÒª"+(i-1)/8+"´Î£»");
 		message.append("9¸öµÄÄÃÒª"+(i)/9+"´Î¡£");
->>>>>>> d4b1f762158692e3a8fec5e5fa07c4081ae5bd4a
 		return message.toString();
 	}
 
